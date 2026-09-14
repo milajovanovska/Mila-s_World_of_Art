fdd39e26b71f9d5c5678b4da3559bcacf0aed2a3
Mila <milajovanovska18@gmail.com>
2026-09-14 11:00:45 +0200

    fix: emailjs service id

--- email-config.js
+++ email-config.js
@@ -1,6 +1,6 @@
 (function () {
     const EMAILJS_PUBLIC_KEY = '1SDDHAD3XovJDCtFz';
-    window.EMAILJS_SERVICE_ID = 'service_jkzbhgl';
+    window.EMAILJS_SERVICE_ID = 'service_xqbt9hf';
     window.EMAILJS_TEMPLATE_ID = 'template_3i3b8ww';
 
     window.CLOUDINARY_CLOUD_NAME = 'ri3c7vsi';
